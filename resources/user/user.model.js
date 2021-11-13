2cb467982e0f3bfe534beba43521f357a1eeb57f
NikosSp <54745415+spiropoulos94@users.noreply.github.com>
2021-11-13 22:45:51 +0200

    added sign in

--- resources/user/user.model.js
+++ resources/user/user.model.js
@@ -1,50 +1,48 @@
 import mongoose from "mongoose";
 import bcrypt from "bcrypt";
 
-const userSchema = new mongoose.Schema({
+const userSchema = new mongoose.Schema(
   {
     email: {
-    type: String,
-    required: true,
-    unique: true,
-    trim: true,
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
   },
+  { timestamps: true }
+);
 
-  password: {
-    type: String,
-    required: true,
+userSchema.pre("save", function (next) {
+  if (!this.isModified("password")) {
+    return next();
   }
-},
-  { timestamps: true },
+
+  bcrypt.hash(this.password, 8, (err, hash) => {
+    if (err) {
+      return next(err);
+    }
+
+    this.password = hash;
+    next();
+  });
 });
 
-userSchema.pre('save', function(next) {
-    if (!this.isModified('password')) {
-      return next()
-    }
-  
-    bcrypt.hash(this.password, 8, (err, hash) => {
+userSchema.methods.checkPassword = function (password) {
+  const passwordHash = this.password;
+  return new Promise((resolve, reject) => {
+    bcrypt.compare(password, passwordHash, (err, same) => {
       if (err) {
-        return next(err)
+        return reject(err);
       }
-  
-      this.password = hash
-      next()
-    })
-  })
 
-  userSchema.methods.checkPassword = function(password) {
-    const passwordHash = this.password
-    return new Promise((resolve, reject) => {
-      bcrypt.compare(password, passwordHash, (err, same) => {
-        if (err) {
-          return reject(err)
-        }
-  
-        resolve(same)
-      })
-    })
-  }
+      resolve(same);
+    });
+  });
+};
 
-
-export const User = mongoose.model('user', userSchema)
+export const User = mongoose.model("user", userSchema);
--- utils/auth.js
+++ utils/auth.js
@@ -2,8 +2,64 @@ import config from "../config/index.js";
 import { User } from "../resources/user/user.model.js";
 import jwt from "jsonwebtoken";
 
+// Create new jwt token
 export const newToken = (user) => {
   return jwt.sign({ id: user.id }, config.SECRETS.JWT_SECRET, {
     expiresIn: config.SECRETS.JWT_EXP,
   });
+};
+
+// Verify token and extract payload
+export const verifyToken = (token) =>
+  new Promise((resolve, reject) => {
+    jwt.verify(token, config.secrets.jwt, (err, payload) => {
+      if (err) return reject(err);
+      resolve(payload);
+    });
+  });
+
+// Sign up
+export const signup = async (req, res) => {
+  if (!req.body.email || !req.body.password) {
+    return res.status(400).send({ message: "need email and password" });
+  }
+
+  try {
+    const user = await User.create(req.body);
+    const token = newToken(user);
+    return res.status(201).send({ token });
+  } catch (e) {
+    return res.status(500).end();
+  }
+};
+
+export const signin = async (req, res) => {
+  if (!req.body.email || !req.body.password) {
+    return res.status(400).send({ message: "need email and password" });
+  }
+
+  const invalidAttempt = { message: "Invalid email and password combination" };
+  const invalidUser = { message: "This user doesnt exist" };
+
+  try {
+    const user = await User.findOne({ email: req.body.email })
+      .select("email password")
+      .exec();
+
+    if (!user) {
+      return res.status(401).send(invalidUser);
+    }
+
+    const match = await user.checkPassword(req.body.password);
+
+    if (!match) {
+      return res.status(401).send(invalidAttempt);
+    }
+
+    const token = newToken(user);
+    return res.status(201).send({ token });
+  } catch (e) {
+    console.error(e);
+    res.status(500).end();
+  }
 };
