ef0ff90ba2981df4b7a831e8edb6f86fb113ddb0
NikosSp <54745415+spiropoulos94@users.noreply.github.com>
2021-11-13 12:40:00 +0200

    added secrets to config

--- config/index.js
+++ config/index.js
@@ -5,6 +5,10 @@ dotenv.config();
 const config = {
   PORT: process.env.PORT || 3000,
   ENV: process.env.NODE_ENV || "development",
+  SECRETS: {
+    JWT_SECRET: process.env.JWT_SECRET || "secret",
+    JWT_EXP: process.env.JWT_EXP || "7d",
+  },
 };
 
 export default config;
