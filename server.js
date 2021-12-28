2c3e91a42e09507b723c86656091f984e7b463a6
NikosSp <54745415+spiropoulos94@users.noreply.github.com>
2021-12-28 22:27:04 +0200

    revert use json

--- server.js
+++ server.js
@@ -20,10 +20,8 @@ app.disable("x-powered-by");
 // });
 
 // app.use(cors());
-app.use(express.json());
-// app.use(express.urlencoded());
 app.use(json());
-// app.use(urlencoded({ extended: true }));
+app.use(urlencoded({ extended: true }));
 app.use(cors({ credentials: true, origin: "http://localhost:8080" }));
 app.use(morgan("dev"));
 app.use(cookieParser());
