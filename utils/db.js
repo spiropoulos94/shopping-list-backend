22714dd1d7a233b1875a0015f503d9f6062fbe69
NikosSp <54745415+spiropoulos94@users.noreply.github.com>
2021-12-29 00:36:13 +0200

    added catch block on mongoose connect

--- utils/db.js
+++ utils/db.js
@@ -2,7 +2,11 @@ import mongoose from "mongoose";
 import config from "../config/index.js";
 
 export const connect = () => {
-  mongoose.connect(config.DB_URL, {
-    useNewUrlParser: true,
-  });
+  mongoose
+    .connect(config.DB_URL, {
+      useNewUrlParser: true,
+    })
+    .catch((e) => {
+      console.log(e);
+    });
 };
